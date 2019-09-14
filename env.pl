79f35e0b542c05b105072cbb3ebb827f89c74e06
Yen-Ming Lee <leeym@leeym.com>
2019-09-14 11:00:42 -0700

    remove AWS_SESSION_TOKEN from env.pl

--- env.pl
+++ env.pl
@@ -1,6 +1,6 @@
 use lib 'local/lib/perl5';
 use Data::Dumper;
-foreach my $key (qw(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY))
+foreach my $key (qw(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN))
 {
   delete($ENV{$key});
 }
