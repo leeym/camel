f70872609e5d99292ac0c1dff48b463e64aaa380
Yen-Ming Lee <leeym@leeym.com>
2019-09-18 00:56:28 -0700

    escape braces in regex

--- wbsc.pl
+++ wbsc.pl
@@ -51,7 +51,7 @@ foreach my $year ($YEAR - 1 .. $YEAR + 1)
       next if $script !~ m{schedule:};
       next if $script !~ m{tournament:};
       my ($s, $t) = (decode_json($1), decode_json($2))
-        if $script =~ m{schedule:\s*({.*?}),\s+tournament:\s*({.*})\s*\};};
+        if $script =~ m{schedule:\s*(\{.*?\}),\s+tournament:\s*(\{.*\})\s*\};};
       foreach my $date (keys $s->{games})
       {
         foreach my $g (@{ $s->{games}->{$date} })
