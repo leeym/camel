e38c50b6fe68d7c59ac9ab8564c25d38c3566964
Yen-Ming Lee <leeym@leeym.com>
2019-10-27 20:58:33 -0700

    handle pagination for api

--- wbsc.pl
+++ wbsc.pl
@@ -18,6 +18,7 @@ my $http = new HTTP::Tiny;
 my $base = 'http://www.wbsc.org';
 my $YEAR = (localtime)[5] + 1900;
 my %URL;
+my %TOURNAMENT;
 
 sub get
 {
@@ -68,6 +69,56 @@ sub tz
     . $country;
 }
 
+sub schedules
+{
+  my $url = shift;
+  my $s   = decode_json(get($url));
+  my $tid = $1 if $url =~ m{/(\d+)/};
+  my $t   = $TOURNAMENT{$tid};
+  foreach my $g (sort { $a->{id} <=> $b->{id} } @{ $s->{data} })
+  {
+    next if $g->{home}->{ioc} ne $team && $g->{away}->{ioc} ne $team;
+    $ENV{TZ} = tz($g, $t);
+    my $score = $g->{away}->{runs} . ':' . $g->{home}->{runs};
+    $score = 'vs' if $score eq '0:0';
+    my $away    = $g->{away}->{label};
+    my $home    = $g->{home}->{label};
+    my $game    = $1 if $g->{label} =~ m{#(\d+)};
+    my $round   = $1 if $g->{label} =~ m{\((.*?)\)};
+    my $summary = "#$game $away $score $home";
+    $summary .= " - $t->{tournamentname}";
+    $summary .= " - $round";
+    $summary =~ s{Chinese Taipei}{Taiwan};
+    warn join(' ', $g->{id}, $g->{start}, $ENV{TZ}, $summary) . "\n";
+    my $boxscore = $t->{external_link} . '/box-score/' . $g->{id};
+    $boxscore =~ s{/en/}{/zh/};
+    my ($yyyy, $mm, $dd, $HH, $MM) = split(/\D/, $g->{start});
+    my $start    = mktime(0, $MM, $HH, $dd, $mm - 1, $yyyy - 1900);
+    my $duration = $g->{duration} || '3:00';
+    my ($hour, $min) = split(/\D/, $duration);
+    $duration = 'PT' . int($hour) . 'H' . int($min) . 'M';
+    my $vevent = Data::ICal::Entry::Event->new();
+    my $video_url =
+      $g->{video_url} || 'https://www.youtube.com/watch?v=' . $g->{video_id};
+    $vevent->add_properties(
+      description => "BOXSCORE: $boxscore\n"
+        . "VIDEO   : $video_url\n"
+        . 'UPDATE  :'
+        . strftime('%FT%T%z', gmtime),
+      dtstart         => Date::ICal->new(epoch => $start)->ical,
+      duration        => $duration,
+      'last-modified' => Date::ICal->new(epoch => time)->ical,
+      location        => $g->{stadium} . ', ' . $g->{location},
+      summary         => $summary,
+      uid             => $g->{id},
+      url             => $boxscore,
+    );
+    $ics->add_entry($vevent);
+  }
+  my $p = $s->{meta}->{pagination};
+  schedules($p->{links}->{next}) if $p->{current_page} < $p->{total_pages};
+}
+
 foreach my $year ($YEAR .. $YEAR + 1)
 {
   foreach
@@ -91,48 +142,10 @@ foreach my $year ($YEAR .. $YEAR + 1)
     foreach my $script (@SCRIPT)
     {
       next if $script !~ m{tournament:};
-      my $t = decode_json($1) if $script =~ m{tournament:\s*(\{.*\})\s*\};};
+      my $t  = decode_json($1) if $script =~ m{tournament:\s*(\{.*\})\s*\};};
       my $id = $t->{id};
-      my $schedules = "https://api.wbsc.org/api/tournaments/$id/schedules?";
-      my $s         = decode_json(get($schedules));
-
-      foreach my $g (sort { $a->{id} <=> $b->{id} } @{ $s->{data} })
-      {
-        next if $g->{home}->{ioc} ne $team && $g->{away}->{ioc} ne $team;
-        $ENV{TZ} = tz($g, $t);
-        my $score = $g->{away}->{runs} . ':' . $g->{home}->{runs};
-        $score = 'vs' if $score eq '0:0';
-        my $away    = $g->{away}->{label};
-        my $home    = $g->{home}->{label};
-        my $game    = $1 if $g->{label} =~ m{#(\d+)};
-        my $round   = $1 if $g->{label} =~ m{\((.*?)\)};
-        my $summary = "#$game $away $score $home";
-        $summary .= " - $t->{tournamentname}";
-        $summary .= " - $round";
-        $summary =~ s{Chinese Taipei}{Taiwan};
-        warn join(' ', $g->{id}, $g->{start}, $ENV{TZ}, $summary) . "\n";
-        my $boxscore = $event . '/box-score/' . $g->{id};
-        $boxscore =~ s{/en/}{/zh/};
-        my ($yyyy, $mm, $dd, $HH, $MM) = split(/\D/, $g->{start});
-        my $start = mktime(0, $MM, $HH, $dd, $mm - 1, $yyyy - 1900);
-        my $duration = $g->{duration} || '3:00';
-        my ($hour, $min) = split(/\D/, $duration);
-        $duration = 'PT' . int($hour) . 'H' . int($min) . 'M';
-        my $vevent = Data::ICal::Entry::Event->new();
-        $vevent->add_properties(
-          description => "$boxscore\n\n"
-            . $g->{video_url} . "\n\n"
-            . strftime('%FT%T%z', gmtime),
-          dtstart         => Date::ICal->new(epoch => $start)->ical,
-          duration        => $duration,
-          'last-modified' => Date::ICal->new(epoch => time)->ical,
-          location        => $g->{stadium} . ', ' . $g->{location},
-          summary         => $summary,
-          uid             => $g->{id},
-          url             => $boxscore,
-        );
-        $ics->add_entry($vevent);
-      }
+      $TOURNAMENT{$id} = $t;
+      schedules("https://api.wbsc.org/api/tournaments/$id/schedules?page=1");
     }
   }
 }
