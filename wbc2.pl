1087829cba8b1603aa280241a3cf1ba30e911ee9
Yen-Ming Lee <leeym@leeym.com>
2023-10-17 06:28:44 -0700

    .wbc2.pl.swp wbc2.pl

--- wbc2.pl
+++ wbc2.pl
@@ -19,7 +19,7 @@ my $ics  = new Data::ICal;
 my $http = Net::Async::HTTP->new(
   max_connections_per_host => scalar(@YEAR),
   max_in_flight            => 0,
-  timeout                  => 10,
+  timeout                  => 20,
 );
 my %VEVENT;
 my $start = time();
