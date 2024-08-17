679124d2f7543747f385e2dca33e32ac369a15b3
Yen-Ming Lee <leeym@leeym.com>
2024-08-16 21:34:11 -0700

    littleleague.pl

--- littleleague.pl
+++ littleleague.pl
@@ -14,13 +14,14 @@ use Time::HiRes qw(time sleep);
 use URL::Builder;
 use strict;
 
-my @YEAR = (2006 .. (localtime)[5] + 1901);
+my @YEAR = (2020 .. (localtime)[5] + 1901);
 my $ics  = new Data::ICal;
 my $http = Net::Async::HTTP->new(
   max_connections_per_host => 0,
   max_in_flight            => 0,
   timeout                  => 20,
 );
+my @FUTURE;
 my %VEVENT;
 my $start = time();
 my %START;
@@ -39,9 +40,27 @@ $MM{October}   = '10';
 $MM{November}  = '11';
 $MM{December}  = '12';
 
+my %LOCATION;
+$LOCATION{llbws} =
+  'Little League International Complex, South Williamsport, PA';
+$LOCATION{jlbws} =
+  'Junior League World Series Field, Heritage Park, Taylor, MI';
+
 IO::Async::Loop->new()->add($http);
 
-event()->get();
+foreach my $year (reverse sort @YEAR)
+{
+  my @TYPE = qw(llbws jlbws);
+  for my $type (@TYPE)
+  {
+    event($year, $type);
+  }
+}
+
+for my $future (@FUTURE)
+{
+  await $future->get();
+}
 
 END
 {
@@ -70,108 +89,94 @@ sub by_dtstart
 sub event
 {
   my $year = shift;
+  my $type = shift;
   my $url  = build_url(
     base_uri => 'https://www.littleleague.org',
-    path     => '/world-series/2024/llbws/tournaments/world-series/',
+    path     => "/world-series/$year/$type/teams/asia-pacific-region/",
   );
 
   return if $START{$url};
   $START{$url} = time;
   warn "get $url\n";
-  return $http->GET($url)->on_done(
+  my $future = $http->GET($url)->on_done(
     sub {
       my $response = shift;
       my $url      = $response->request->url;
       my $elapsed  = int((time - $START{$url}) * 1000);
       my $html     = $response->content;
-      $html =~ s{\r}{}g;
-      $html =~ s{\n}{}g;
-      $html =~ s{>\s+<}{><}g;
-      $html =~ s{.*#schedule}{}g;
+      $html         =~ s{\r}{}g;
+      $html         =~ s{\n}{}g;
+      $html         =~ s{\s+}{ }g;
+      $html         =~ s{>\s+<}{><}g;
+      next if $html !~ m{Chinese Taipei};
+      my @GAME = split(/\bws-card\b/, $html);
+      shift @GAME;
       my $n;
-      my @DATE = split(/site-panel--game-schedule__heading/, $html);
-      shift @DATE;
 
-      for my $d (@DATE)
+      for my $g (@GAME)
       {
-        my $date = $1      if $d    =~ m{(\w+ \d{1,2}, \d{4})};
-        my $mm   = $MM{$1} if $date =~ m{^(\w+) };
-        my $dd   = $1      if $date =~ m{(\d{1,2}),};
-        my $yyyy = $1      if $date =~ m{(\d{4})};
-        my @GAME = split(/g-col/, $d);
-        shift @GAME;
-        for my $g (@GAME)
+        my ($header, $content, $footer) = ($1, $2, $3)
+          if $g =~ m{(<header.*?/header>)(.*?)(<footer.*?/footer>).*};
+        next if !$header || !$content || !$footer;
+        next if $header !~ m{Game};
+
+        my $title = $1 if $header =~ m{<h3 class="ws-card__title">(.*?)</h3>};
+        my $game  = $1 if $header =~ m{>(Game.*?)<};
+        my $info  = $1 if $header =~ m{.*>(.*?M.*?)<};
+        my @INFO     = split(/@/, $info);
+        my $datetime = $INFO[0];
+
+        my $hh = sprintf("%02d", $1) if $datetime =~ m{(\d+)};
+        my $mm = sprintf("%02d", $2) if $datetime =~ m{(\d+):(\d{2})};
+        $hh += 12 if $datetime =~ m{PM} && $hh != 12;
+        my $mm = $MM{$1}             if $datetime =~ m{ ([A-Z][a-z]*) \d+};
+        my $dd = sprintf("%02d", $1) if $datetime =~ m{(\d{1,2})};
+
+        my $ical = $year . $mm . $dd . 'T' . $hh . $mm . '00';
+        my $summary;
+        my $away;
+        my $home;
+        my $result;
+        my @LI = split(/<\/li>/, $content);
+
+        for my $li (@LI)
         {
-          next if $g !~ m{Asia-Pacific};
-          $g =~ s{Asia-Pacific}{Taiwan}g;
-          my ($header, $content, $footer) = ($1, $2, $3)
-            if $g =~ m{(<header.*/header>)(.*)(<footer.*/footer>)};
-          my $header = $1 if $g =~ m{<header.*?>(.*)</header>};
-
-          my $title = $1 if $header =~ m{<h3 class="ws-card__title">(.*?)</h3>};
-          my $game  = $1 if $header =~ m{>(Game.*?)<};
-          my $info  = $1 if $header =~ m{.*>(.*?Stadium)<};
-          my @INFO     = split(/@/, $info);
-          my $datetime = $INFO[0];
-          my $location = $INFO[1];
-
-          my $hh = $1 if $datetime =~ m{^(\d)};
-          $hh += 12   if $datetime =~ m{\d+ PM};
-
-          my $ical = $yyyy . $mm . $dd . 'T' . $hh . '0000';
-          warn $ical;
-          my $summary;
-          my $away;
-          my $home;
-          my $result;
-          my @LI = split(/<\/li>/, $content);
-          for my $li (@LI)
+          last if $away && $home;
+          my $team  = $1 if $li =~ m{<h4.*>(.*?) Region</h4>};
+          my $score = $1 if $li =~ m{score">(\d+)</div>};
+          if (!$away)
           {
-            next           if $away && $home;
-            my $team = $1  if $li =~ m{<h4.*>(.*?) Region</h4>};
-            die $li        if !$team;
-            my $score = $1 if $li =~ m{score">(\d+)</div>};
-            if (!$away)
-            {
-              $away   = $team;
-              $result = $score;
-            }
-            else
-            {
-              $home = $team;
-              $result .= ":$score";
-            }
+            $away   = $team;
+            $result = $score;
           }
-          $result = 'vs' if $result eq ':';
-          $summary .= "$away $result $home | $title $game";
-
-          my $watch = $1
-            if $footer =~ m{"(https://www.espn.com/watch/[^"]*)"};
-          my $lineups = $1
-            if $footer =~
-            m{"(https://www.littleleague.org/downloads/[^"]*?-lineups/)"};
-          my $boxscore = $1
-            if $footer =~
-            m{"(https://www.littleleague.org/downloads/[^"]*?-box-score/)"};
-
-          my $description = "* $watch\n* $lineups\n* $boxscore\n";
-          my $vevent      = Data::ICal::Entry::Event->new();
-          my $epoch       = time;
-          $vevent->add_properties(
-            description => $description,
-            dtstart  => Date::ICal->new(ical => $ical, offset => '-0400')->ical,
-            duration => 'PT3H0M',
-            'last-modified' => Date::ICal->new(epoch => time)->ical,
-            location        => $location . ', South Williamsport, PA 17702',
-            summary         => $summary,
-            id              => $game,
-            url             => $boxscore,
-          );
-          $VEVENT{$game} = $vevent;
-          $n++;
+          else
+          {
+            $home = $team;
+            $result .= ":$score";
+          }
         }
+        $result = 'vs' if $result eq ':';
+        $summary .= "$away $result $home | $title $game";
+
+        my $links = $1 if $footer =~ m{(<ul.*?/ul>)};
+
+        my $lastmodified = Date::ICal->new(epoch => time)->ical;
+        my $description  = "* $lastmodified\n$links";
+        my $vevent       = Data::ICal::Entry::Event->new();
+        my $epoch        = time;
+        $vevent->add_properties(
+          description => $description,
+          dtstart  => Date::ICal->new(ical => $ical, offset => '-0400')->ical,
+          duration => 'PT3H0M',
+          'last-modified' => Date::ICal->new(epoch => time)->ical,
+          location        => $LOCATION{$type},
+          summary         => $summary,
+        );
+        $VEVENT{$game} = $vevent;
+        $n++;
       }
       warn "got $url ($n events, $elapsed ms)\n";
     }
   );
+  push(@FUTURE, $future);
 }
