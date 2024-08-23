508de001c86899799d914c5a6902d587a0ad1266
Yen-Ming Lee <leeym@leeym.com>
2024-08-22 21:03:00 -0700

    littleleague.pl olympics.pl pony.pl wbc.pl wbsc.pl

--- wbsc.pl
+++ wbsc.pl
@@ -246,7 +246,7 @@ print $ics->as_string;
 
 END
 {
-  die $? if !scalar(keys(%VEVENT));
+  die $@ if $@;
   warn "Total: " . scalar(keys %VEVENT) . " events\n";
   warn "Duration: " . int((time - $start) * 1000) . " ms\n";
 }
