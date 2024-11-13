c78e5db9a0672c08f1724f755a056df5f595e2a9
Yen-Ming Lee <leeym@leeym.com>
2024-11-13 03:31:58 -0800

    wbc.pl

--- wbc.pl
+++ wbc.pl
@@ -112,10 +112,10 @@ sub event
           my $score = sprintf('%d:%d',
             $g->{teams}->{away}->{score},
             $g->{teams}->{home}->{score});
-          my $summary = sprintf("%s %s %s | World Baseball Classic %s - %s",
-            $away, $score, $home, $g->{season}, $g->{description});
           my $epoch = str2time($g->{gameDate});
           $score = 'vs' if $epoch > time;
+          my $summary = sprintf("%s %s %s | World Baseball Classic %s - %s",
+            $away, $score, $home, $g->{season}, $g->{description});
 
           warn $g->{gameDate} . " $summary\n";
           my $gameday = 'https://www.mlb.com/gameday/' . $g->{gamePk};
